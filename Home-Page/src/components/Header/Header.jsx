37a75059dc8d74cad4c7ce28cbda38da44d3a21d
Walter-Britos1 <walterbritos418@gmail.com>
2024-04-23 03:20:39 -0300

    logo created

--- Home-Page/src/components/Header/Header.jsx
+++ Home-Page/src/components/Header/Header.jsx
@@ -1,16 +1,11 @@
-import { Search } from 'lucide-react';
-import { CircleUserRound } from 'lucide-react';
-import { ShoppingCart } from 'lucide-react';
+import Logo from '../Logo/Logo';
+import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <div>
-      <h1>Mi Tienda</h1>
-      <input type="search" />
-      <Search size={24} />
-      <p>Mi cuenta</p>
-      <CircleUserRound size={24} />
-      <ShoppingCart size={24} />
+    <div className={styles.container}>
+      <Logo />
     </div>
   )
 }
+
---
+++ Home-Page/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import styles from './Logo.module.css'
+
+export default function Logo() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.paragraph}>
+        Mi Tienda
+      </p>
+    </div>
+  )
+}
