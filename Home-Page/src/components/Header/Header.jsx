b5e992254ae4c56e877f85aec3c06840012ae80c
Walter-Britos1 <walterbritos418@gmail.com>
2024-04-23 05:03:50 -0300

    creating searchbar

--- Home-Page/src/components/Header/Header.jsx
+++ Home-Page/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import Logo from '../Logo/Logo';
+import SearchBar from '../SearchBar/SearchBar';
 import styles from './Header.module.css'
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <Logo />
+      <SearchBar />
     </div>
   )
 }
