7b2b14c967548f46af3c0b5e59dc81e6f90ff7a9
Walter-Britos1 <walterbritos418@gmail.com>
2024-04-24 02:11:42 -0300

    finished main image

--- Home-Page/src/App.jsx
+++ Home-Page/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header"
 import NavBar from "./components/NavBar/NavBar"
+import ImgMain from "./components/ImgMain/ImgMain"
 
 function App() {
   return (
     <div>
       <Header />
       <NavBar />
+      <ImgMain />
     </div>
   )
 }
